88cf7a8341bca6f085a37d96d7df08eb83c05035
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2024-11-28 07:46:42 -0300

    Alterado texto FindSign

--- src/pages/findsign.vue
+++ src/pages/findsign.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="d-flex justify-content-between" style="background-color: #1250ac; min-height: 75vh">
+    <div class="d-flex justify-content-between pb-4" style="background-color: #1250ac; min-height: 75vh">
       <div class="col-md-6">
         <div style="margin-top: 50px">
           <div
@@ -17,24 +17,31 @@
               style="text-align: justify"
               v-if="!chooseContent"
             >
-              E se nós pudéssemos ter uma grande nuvem de dados para consultar,
-              gratuita e virtualmente, vários sinais da Libras de diversas áreas
-              do conhecimento, criados em resposta a demandas de vários
-              contextos?<br /><br /></p>
+              E se nós pudéssemos ter uma grande nuvem de dados para consultar de maneira gratuita, virtual e acessível?
+              Pesquisar vários sinais da Libras de diversas áreas diferentes
+              do conhecimento, criados pelo Brasil afora e também aqui no contexto interno do UDF,
+              com o objetivo de responder de maneira concreta a falta de sinais e termos específicos na língua?
+              Então, fazer isso aqui é possível sim!<br /><br /></p>
 
               <p
                 class="ps-4 ms-4 mt-2 fs-6 text-white"
                 style="text-align: justify"
                 v-if="!chooseContent"
               >
-              Essa é a proposta do repositório FindSign - Sinais e-Clouds - um
-              ambiente on-line voltado ao armazenamento de fontes de conteúdos
-              em Libras sobre assuntos presentes nos diversos contextos
-              acadêmicos, para auxiliar o usuário no manejo de dicionários
-              on-line de especialidade, orientando pesquisas documentais e dando
-              suporte à aprendizagem científica de conteúdos terminológicos
-              complexos por estudantes universitários surdos e ouvintes usuários
-              da Libras.
+              A nossa proposta no FindSign é oferecer um ambiente on-line, aplicado ao
+              armazenamento de fontes de consulta variadas em libras sobre conteúdos de ensino
+              e aprendizagem diversos, próprios de conceitos educacionais acadêmicos, de cursos
+              de nível superior aqui do UDF, em Brasília, para auxiliar o estudante, seja surdo ou ouvinte,
+              a usar de maneira concreta dicionários on-line de sinais/termos referentes a conteúdos terminológicos 
+              complexos do contexto acadêmicos de estudos. Inicialmente, o <strong>FindSign</strong> vai oferecer oportunidades de consulta de cursos daqui
+              do UDF, em que há estudantes surdos e também cursos em que os conteúdos já estão oferecidos em libras.
+              O cursos estão listados aqui, e são eles:
+              <ul>
+                <li><strong>Tecnologia da Informação</strong></li>
+                <li><strong>Medicina Veterinária</strong></li>
+                <li><strong>Nutrição</strong></li>
+                <li><strong>Negócios</strong></li>
+              </ul>
             </p>
             <p
               v-else
@@ -74,7 +81,7 @@
       <div class="col-sm-4">
         <div class="custom-iframe" style="align-items: end; margin-top: 74px">
           <iframe
-            src="https://www.youtube.com/embed/EZxkymw426U"
+            src="https://www.youtube.com/embed/pGdL9wgy6E8"
             title="Alfabeto de LIBRAS - COMPLETO E INESQUECÍVEL"
             class="border"
             frameborder="0"
@@ -144,7 +151,7 @@ useHead({
   transform: scale(1.2);
   transition: all 0.5s ease;
   box-shadow: 0 1rem 3rem rgb(174, 174, 174);
-  background-color: #0d6efd;
+  background-color: #00AEFF;
 }
 
 .curso-btn:not(:hover) {
@@ -155,13 +162,13 @@ useHead({
 
 .curso-btn:focus {
   transition: all 0.5s ease;
-  background-color: #0d6efd;
+  background-color: #00AEFF;
   transform: scale(1.2);
   box-shadow: 0;
 }
 
 .custom-iframe iframe {
-  height: 300px;
+  height: 450px;
   width: 80%;
 }
 
